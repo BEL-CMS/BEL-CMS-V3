ca4bb5c4f5d891f140b867411021accf8c7fecc6
Stive <stivedeterme@msn.com>
2024-05-16 20:04:02 +0200

    + registered by email
    + registered by email
    + html user registered

--- core/class.mail.php
+++ core/class.mail.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Bel-CMS [Content management system]
- * @version 3.0.1 [PHP8.3]
+ * @version 3.0.3 [PHP8.3]
  * @link https://bel-cms.dev
  * @link https://determe.be
  * @license http://opensource.org/licenses/GPL-3.-copyleft
@@ -12,6 +12,7 @@
 namespace BelCMS\Core;
 
 use DateTime;
+use BelCMS\Requires\Common;
 
 if (!defined('CHECK_INDEX')):
     header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
@@ -22,92 +23,106 @@ final class eMail
 {
 	public	$fromName,
 			$fromMail,
+			$toMail,
+			$SenderName,
 			$subject,
+			$header,
+			$body,
+			$footer,
 			$content,
 			$sendMail,
-			$msg;
+			$html;
 	private $date;
 
 	public function __construct()
 	{
 		$this->date = new DateTime('now');
 		$this->date->format('d/m/Y à H:i:s');
+		if (!defined("PHP_EOL_MAIL")) define("PHP_EOL_MAIL", "\r\n");
+		$this->fromName   = self::fromName();
+		$this->fromMail   = self::fromMail();
+		$this->toMail     = self::toMail();
+		$this->SenderName = self::SenderName();
+		$this->subject    = self::subject();
+		$this->header     = self::header();
+		$this->body       = self::body();
+		$this->footer     = self::footer();
 	}
 
     public function fromName ($data = false)
 	{
-		$this->fromName = !empty($data) ? $data : $_SESSION['CONFIG_CMS']['CMS_WEBSITE_NAME'];
+		if (!empty($data)) {
+			$this->fromName($data);
+		} else {
+			Notification::error('Erreur, Erreur');
+		}
 	}
 
 	public function fromMail ($data = false)
 	{
-		$this->fromMail = !empty($data) ? $data : $_SERVER['SERVER_ADMIN'];
+		return !empty($data) ? Secure::isMail($data) : $_SERVER['SERVER_ADMIN'];
 	}
 
-	public function subject ($data = false)
+	public function toMail ($data = null)
 	{
-		$this->subject = !empty($data) ? $data : '';
+		return !empty($data) ? Secure::isMail($data) : '';
 	}
 
-	public function message ($data = false)
+	public function SenderName ($data = null)
 	{
-		$this->msg = !empty($data) ? $data : false;
+		return !empty($data) ? Common::VarSecure($data, 'null') : '';
 	}
 
-	public function output ($data)
+	public function subject ($data = null)
 	{
-		$this->sendMail = !empty($data) ? $data : false;
+		$this->subject = !empty($data) ? Common::VarSecure($data, 'null') : '';
 	}
 
-	public function html ($data = false)
+	public function onYourMind ($data = null)
 	{
-		$this->content  = !empty($data) ? $data : false;
-		if ($this->content === false) {
-			$this->content = '
-			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-			<html xmlns:v="urn:schemas-microsoft-com:vml">
-				<head>
-					<meta http-equiv="content-type" content="text/html; charset=utf-8">
-					<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-				</head>
-				<body style="width: 100%;height: 100%;background: #37383a;">
-					<table cellpadding="0" cellspacing="0" style="width: 620px; margin:0 auto;">
-						<tr><td style="height: 10px; background-color: #5187bd;border-collapse:collapse; text-align:left;"></td></tr>
-						<tr style="background: #FFF;">
-							<td style="padding: 25px;font-size: 24px;color: #5187bd;font-family: Helvetica, sans-serif;">'.$this->fromName.'</td>
-						</tr>
-						<tr><td style="height: 3px; background: #777777;border: 1px dotted #777777;padding: 2px 0; background: #FFF;"></td></tr>
-						<tr>
-							<td style="background: #5187bd;color:#FFF;line-height: 90px;padding: 5px 25px;font-size: 24px;">Formulaire de contact</td>
-						</tr>
-						<tr><td style="height: 3px; background: #777777;border: 1px dotted #777777;padding: 2px 0; background: #FFF;"></td></tr>
-						<tr style="background: #FFF;">
-							<td style="color:#aaaaaa;padding: 25px 0 0 25px;font-size: 12px;font-family:Arial, Helvetica, sans-serif;">'.$this->date.'</td>
-						</tr>
-						<tr style="background: #FFF;">
-							<td style="padding: 0 25px 0px 25px;font-family:"Segoe UI", "Helvetica Neue", Helvetica, Arial, sans-serif; font-size:36px;font-size: 24px;color: #777777;">'.$this->subject.'</td>
-						</tr>
-						<tr style="background: #FFF;">
-							<td style="padding: 5px 25px 25px 25px;text-align: justify;font-family: Arial, Helvetica, sans-serif;font-size: 13px;line-height: 15pt;color: #777777;">
-								'.$this->msg.'
-							</td>
-						</tr>
-						<tr><td style="height: 10px;background-color: #5187bd;border-collapse:collapse; text-align:left;"></td></tr>
-					</table>     
-				</body>
-			</html>';
+		if ($data != null) {
+			return Common::VarSecure($data, 'html');
+		} else {
+			return '';
 		}
+	}
+
+	public function header ($data = null)
+	{
+		$this->header = Common::VarSecure($data, 'html');
+	}
+
+	public function body ($data = null)
+	{
+		$this->body = Common::VarSecure($data, 'html');
+	}
+
+	public function footer ($data = null)
+	{
+		$this->footer = Common::VarSecure($data, 'html');
 	}
 
 	public function send ()
 	{
-		$headers   = array();
-		$headers[] = "MIME-Version: 1.0";
-		$headers[] = 'Content-Type: text/html; charset=iso-8859-1';
-		$headers[] = "From: {$this->fromName} <{$this->fromMail}>";
-		$headers[] = "Reply-To: NoReply <{$this->fromMail}>";
-		$headers[] = "X-Mailer: PHP/".phpversion();
-		$return = mail($this->sendMail, $this->subject, $this->content, implode("\n", $headers));
-		return $return;
+		$name     = $this->fromName();
+		$email    = $this->fromMail();
+
+		$address = "firefox@determe.be";
+		
+
+		$e_body    = stripslashes($this->header);
+		$e_content = stripslashes($this->body);
+		$e_reply   = stripslashes($this->footer);
+		
+		$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+
+
+		
+		$headers  = "From: $email" . PHP_EOL_MAIL;
+		$headers .= "Reply-To: $email" . PHP_EOL_MAIL;
+		$headers .= "MIME-Version: 1.0" . PHP_EOL_MAIL;
+		$headers .= "Content-Type: text/html; charset=utf-8" . PHP_EOL_MAIL;
+		$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL_MAIL;
+		return (mail($address, $this->subject, $msg, $headers));
 	}
 }
--- pages/user/models.php
+++ pages/user/models.php
@@ -17,6 +17,7 @@ use BelCMS\PDO\BDD;
 use BelCMS\User\User as Users;
 use BelCMS\Requires\Common;
 use BelCMS\Core\eMail;
+use DateTimeImmutable;
 
 if (!defined('CHECK_INDEX')):
     header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
@@ -141,7 +142,34 @@ final class User
 							'hash_key'          => $hash_key,
 							'user_group'        => 2,
 							'user_groups'       => 2
-						);	
+						);
+
+						$mail = new eMail;
+
+						$fromName = $_SESSION['CONFIG_CMS']['CMS_WEBSITE_NAME'];
+						if (!empty($_SESSION['CONFIG_CMS']['CMS_MAIL_WEBSITE'])) {
+							$fromMail = $_SESSION['CONFIG_CMS']['CMS_MAIL_WEBSITE'];
+						} else {
+							$fromMail = $_SERVER['SERVER_ADMIN'];
+						}
+						$toMail     = $insertUser['mail'];
+						$SenderName = $_SERVER['SERVER_NAME'];
+						$subject    = 'Enregistrement du compte';
+						$header     = self::sendMailHtml()['header'];
+						$body       = self::sendMailHtml()['body'];
+						$footer     = self::sendMailHtml()['footer'];
+
+						$mail->fromName   = $fromName;
+						$mail->fromMail   = $fromMail;
+						$mail->toMail     = $toMail;
+						$mail->SenderName = $SenderName;
+						$mail->subject    = $subject;
+						$mail->header     = $header;
+						$mail->body       = $body;
+						$mail->footer     = $footer;
+
+						$mail->send();
+
 					}
 					$insertGrp = New BDD();
 					$insertGrp->table('TABLE_USERS_GROUPS');
@@ -172,24 +200,7 @@ final class User
 					$insertPage->table('TABLE_USERS_PAGE');
 					$insertPage->insert(array('hash_key'=> $hash_key));
 
-					if ($_SESSION['CONFIG_CMS']['VALIDATION'] == 'mail' and $test->data != 0) {
-
-						$dataMail['author']  = $_SESSION['USER']->user->username;
-						$dataMail['subject'] = constant('SUBJECT_HTML');
-						$dataMail['message'] = $pass_key;
-						$dataMail['mail']    = $insert->sqlInsert['mail'];
-
-						$mail = new eMail();
-						$mail->fromName($_SESSION['USER']->user->username);
-						$mail->fromMail($_SESSION['CONFIG_CMS']['CMS_MAIL_WEBSITE']); 
-						$mail->subject(constant('SUBJECT_HTML'));
-						$mail->message($pass_key);
-						$mail->output($insert->sqlInsert['mail']);
-						$mail->html(self::html($dataMail));
-						$mail->send();
-
-						Users::login($data['username'],$data['passwordhash']);
-					}
+					//Users::login($data['username'],$data['passwordhash']);
 				}
 			}
 			$return['msg']  = constant('CURRENT_RECORD');
@@ -198,61 +209,26 @@ final class User
 		}
 	}
 
-	private function html ($data)
+	private function sendMailHtml ()
 	{
-		$return = ' 
-		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-		<html xmlns:v="urn:schemas-microsoft-com:vml">
-			<head>
-				<meta http-equiv="content-type" content="text/html; charset=utf-8">
-				<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-			</head>
-			<body style="width: 100%;height: 100%;background: #37383a;text-align: center;">
-				<table cellpadding="0" cellspacing="0" style="width: 620px; margin:0 auto;">
-					<tr>
-						<td style="height: 10px; background-color: #5187bd;border-collapse:collapse; text-align:left;"></td>
-					</tr>
-					<tr style="background: #FFF;">
-						<td style="padding: 25px;font-size: 24px;color: #5187bd;font-family: Helvetica, sans-serif;">'.$_SESSION['CONFIG_CMS']['CMS_WEBSITE_NAME'].'</td>
-					</tr>
-					<tr>
-						<td style="height: 3px; background: #777777;border: 1px dotted #777777;padding: 2px 0; background: #FFF;"></td>
-					</tr>
-					<tr>
-						<td style="background: #5187bd;color:#FFF;padding: 15px 25px;font-size: 24px;">Formulaire de contact</td>
-					</tr>
-					<tr>
-						<td style="height: 3px; background: #777777;border: 1px dotted #777777;padding: 2px 0; background: #FFF;"></td>
-					</tr>
-					<tr style="background: #FFF;">
-						<td style="color:#aaaaaa;padding: 25px 0 0 25px;font-size: 12px;font-family:Arial, Helvetica, sans-serif;">17-04-2024 @ 07h53</td>
-					</tr>
-					<tr style="background: #FFF;">
-						<td style="padding: 0 25px 0px 25px;font-family:Segoe UI, Helvetica Neue, Helvetica, Arial, sans-serif; font-size:36px;font-size: 24px;color: #777777;">'.$data['subject'].'</td>
-					</tr>
-					<tr style="background: #FFF;">
-						<td style="padding: 5px 25px 25px 25px;text-align: justify;font-family: Arial, Helvetica, sans-serif;font-size: 13px;line-height: 15pt;color: #777777;">'.$data['message'].'</td>
-					</tr>
-					<tr style="width: 100%;">
-						<td style="width:100%;">
-							<table width="100%" border="0" cellpadding="0" cellspacing="0" style="text-align: left;font-size: 12px;line-height: 15pt;color: #777777;width: 100%;">
-								<tr style="background: #f4f4f4;width: 100%;">
-									<td style="text-align: center;padding: 15px;color: #777777;font-weight: bold;font-size: 16px;">Information</td>
-								</tr>
-								<tr style="background: #f4f4f4;width: 100%;">
-									<td style="padding: 0 25px;"><strong>Nom</strong> : '.$data['author'].'</td>
-								</tr>
-								<tr style="background: #f4f4f4;width: 100%;">
-									<td style="padding: 0 25px 15px 25px;"><strong>E-mail</strong> : '.$data['mail'].'</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-					<tr><td style="height: 10px;background-color: #5187bd;"></td></tr>
-				</table>     
-			</body>
-		</html>';
-		return $return;
+		$date = new \DateTime();
+
+		if ($_SERVER['SERVER_PORT'] == '80') {
+			$host = 'http://'.$_SERVER['HTTP_HOST'].'/';
+		} else {
+			$host = 'https://'.$_SERVER['HTTP_HOST'].'/';
+		}
+
+		$array['header'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+		<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Clé d\'activation</title>
+		</head>
+		<body>';
+		$array['body'] = '<table width="100%" border="0" cellspacing="5" cellpadding="5" bgcolor="#666666"><thead><tr><th><a style="color:#CCC; text-decoration:none" href="'.$host.'" style="display:block; text-align:center">'.$_SESSION['CONFIG_CMS']['CMS_WEBSITE_NAME'].'</a></th></tr></thead><tbody><tr><td><table width="90%" border="0" align="center" cellpadding="5" cellspacing="5" bgcolor="#FFF"><tr><td><p>Voici la clé de sécurité pour activer votre compte.</p></td></tr></table></td></tr></tbody></table><table style="color:#FFF; text-align:center" width="100%" border="0" align="center" cellpadding="5" cellspacing="5" bgcolor="#3333"><tr><td>c14b296b72fac989752dc55bef211f42</td></tr></table><table style="color:#FFF; text-align:center" width="100%" border="0" align="center" cellpadding="5" cellspacing="5" bgcolor="#8f8e8c"><thead><tr><td colspan="2"><b>Information</b></td></tr></thead><tbody><tr bordercolor="#FFF"><td style="text">Nom</td><td><b>username</b></td></tr><tr><td>date</td><td><b>16 mai 2024</b></td></tr><tr><td>IP</td><td><b>'.Common::GetIp().'</b></td></tr></tbody></table> ';
+		$array['footer'] = '</body></html>';
+		return $array;
 	}
 
 	public function sendEditProfil ($data) {
