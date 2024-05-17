bfa4233f526ac531b85dbe505918f35711f22af7
Stive <stivedeterme@msn.com>
2024-05-17 14:05:03 +0200

    + update class.mail.php
    + update class mail (functional)

--- core/class.mail.php
+++ core/class.mail.php
@@ -13,6 +13,7 @@ namespace BelCMS\Core;
 
 use DateTime;
 use BelCMS\Requires\Common;
+use BelCMS\Core\Secure;
 
 if (!defined('CHECK_INDEX')):
     header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
@@ -24,7 +25,7 @@ final class eMail
 	public	$fromName,
 			$fromMail,
 			$toMail,
-			$SenderName,
+			$senderName,
 			$subject,
 			$header,
 			$body,
@@ -38,53 +39,42 @@ final class eMail
 	{
 		$this->date = new DateTime('now');
 		$this->date->format('d/m/Y à H:i:s');
+
 		if (!defined("PHP_EOL_MAIL")) define("PHP_EOL_MAIL", "\r\n");
+
 		$this->fromName   = self::fromName();
 		$this->fromMail   = self::fromMail();
 		$this->toMail     = self::toMail();
-		$this->SenderName = self::SenderName();
+		$this->senderName = self::senderName();
 		$this->subject    = self::subject();
 		$this->header     = self::header();
 		$this->body       = self::body();
 		$this->footer     = self::footer();
 	}
 
-    public function fromName ($data = false)
+    public function fromName ($data = null)
 	{
-		if (!empty($data)) {
-			$this->fromName($data);
-		} else {
-			Notification::error('Erreur, Erreur');
-		}
+		$this->fromName = !empty($data) ? Common::VarSecure($data, null) : $_SESSION['CONFIG_CMS']['CMS_WEBSITE_NAME'];
 	}
 
-	public function fromMail ($data = false)
+	public function fromMail ($data = null)
 	{
-		return !empty($data) ? Secure::isMail($data) : $_SERVER['SERVER_ADMIN'];
+		$this->fromMail = !empty($data) ? Secure::isMail($data) : $_SERVER['SERVER_ADMIN'];
 	}
 
 	public function toMail ($data = null)
 	{
-		return !empty($data) ? Secure::isMail($data) : '';
+		$this->toMail = !empty($data) ? Secure::isMail($data) : '';
 	}
 
-	public function SenderName ($data = null)
+	public function senderName ($data = null)
 	{
-		return !empty($data) ? Common::VarSecure($data, 'null') : '';
+		$this->senderName = !empty($data) ? Common::VarSecure($data, 'null') : '';
 	}
 
 	public function subject ($data = null)
 	{
 		$this->subject = !empty($data) ? Common::VarSecure($data, 'null') : '';
-	}
-
-	public function onYourMind ($data = null)
-	{
-		if ($data != null) {
-			return Common::VarSecure($data, 'html');
-		} else {
-			return '';
-		}
 	}
 
 	public function header ($data = null)
@@ -104,21 +94,17 @@ final class eMail
 
 	public function send ()
 	{
-		$name     = $this->fromName();
-		$email    = $this->fromMail();
-
-		$address = "firefox@determe.be";
+		$name     = $this->fromName;
+		$email    = $this->fromMail;
+		$address  = $this->toMail;
 		
-
 		$e_body    = stripslashes($this->header);
 		$e_content = stripslashes($this->body);
 		$e_reply   = stripslashes($this->footer);
 		
 		$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
-
-		
-		$headers  = "From: $email" . PHP_EOL_MAIL;
+		$headers  = "From: $name" . PHP_EOL_MAIL;
 		$headers .= "Reply-To: $email" . PHP_EOL_MAIL;
 		$headers .= "MIME-Version: 1.0" . PHP_EOL_MAIL;
 		$headers .= "Content-Type: text/html; charset=utf-8" . PHP_EOL_MAIL;
